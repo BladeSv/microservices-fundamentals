962a5f6d98c54a534e1f4d7fb2f5bfc1356c502a
Mikhail Mitrakhovich <bladesvet@gmail.com>
2024-01-02 15:37:47 +0500

    changed docker image builder

--- resource-processor/src/main/java/by/mitrakhovich/resourceprocessor/message/MessageReceiver.java
+++ resource-processor/src/main/java/by/mitrakhovich/resourceprocessor/message/MessageReceiver.java
@@ -6,17 +6,17 @@ import by.mitrakhovich.resourceprocessor.processor.SongProcessor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
 import java.io.ByteArrayInputStream;
-
 
 @Component
 @Slf4j
@@ -42,13 +42,12 @@ public class MessageReceiver {
         sentSongMetaData(song);
     }
 
-
     private byte[] getResourceObject(String resourceId) {
         log.info("send request to resource service record with id-{}",resourceId);
         RestTemplate restTemplate = new RestTemplate();
         String url = resourceServiceUrl + resourceId;
         byte[] forObject = restTemplate.getForObject(url, byte[].class);
-        log.info("get from resource service record with data length-{}", forObject.length);
+        log.info("get from resource service record with data length-{}",forObject != null? forObject.length : "zero");
         return forObject;
     }
 
@@ -60,7 +59,7 @@ public class MessageReceiver {
         String songJSON = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(song);
 
         HttpEntity<String> request =
-                new HttpEntity<String>(songJSON, headers);
+                new HttpEntity<>(songJSON, headers);
         log.info("send request to song service with url {} song-{}", songServiceUrl, song.toString());
         restTemplate.postForObject(songServiceUrl, request, String.class);
     }
